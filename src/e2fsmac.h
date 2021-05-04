51c4b6e0c186fc93e04eb7fe6c3baf2eee52ad57
xnscdev <xnscdev@gmail.com>
2021-05-04 14:52:45 -0700

    Added ext2 block group and inode structs

--- src/e2fsmac.h
+++ src/e2fsmac.h
@@ -101,6 +101,40 @@ struct ext2_super_block
   uint8_t s_padding3[760];
 };
 
+struct ext2_block_group
+{
+  uint32_t bg_block_bitmap;
+  uint32_t bg_inode_bitmap;
+  uint32_t bg_inode_table;
+  uint16_t bg_free_blocks_count;
+  uint16_t bg_free_inodes_count;
+  uint16_t bg_used_dirs_count;
+  uint16_t bg_pad;
+  uint8_t bg_reserved[12];
+};
+
+struct ext2_inode
+{
+  uint16_t i_mode;
+  uint16_t i_uid;
+  uint32_t i_size;
+  uint32_t i_atime;
+  uint32_t i_ctime;
+  uint32_t i_mtime;
+  uint32_t i_dtime;
+  uint16_t i_gid;
+  uint16_t i_links_count;
+  uint16_t i_blocks;
+  uint32_t i_flags;
+  uint32_t i_osd1;
+  uint32_t i_block[15];
+  uint32_t i_generation;
+  uint32_t i_file_acl;
+  uint32_t i_dir_acl;
+  uint32_t i_faddr;
+  uint8_t i_osd2[12];
+};
+
 struct ext2_mount
 {
   uint32_t magic;
